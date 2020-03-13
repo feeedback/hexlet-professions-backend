67e8b8593ee8e837854ab77ae91ba7a3361b0508
A. Rzhanitsyn <risdeep@gmail.com>
2020-03-13 21:42:14 +0500

    js-testing №4 lesson

---
+++ lessons/js-testing/jest.js
@@ -0,0 +1,27 @@
+// sc: https://ru.hexlet.io/courses/js-testing/lessons/jest/exercise_unit
+
+// tests/take.test.js
+
+// Напишите тесты для функции without(coll, [values]), которая принимает первым параметром
+// массив и возвращает его копию, из которой исключены значения, переданные вторым и
+// последующими параметрами.
+
+// _.without([2, 1, 2, 3], 1, 2); // [3]
+
+// Подсказки
+// _.without https://lodash.com/docs/4.17.15#without
+
+// @ts-check
+
+const getFunction = require('../functions');
+
+const without = getFunction();
+
+// BEGIN (write your solution here)
+test('without', () => {
+    const arr = [2, 1, 2, 3];
+    expect(without(arr, 1, 2)).toEqual([3]);
+    expect(without(arr)).toEqual(arr);
+    expect(without([])).toEqual([]);
+});
+// END
