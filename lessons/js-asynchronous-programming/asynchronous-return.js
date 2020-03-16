1411b129642ceceed6e721b551ee9e55d8cf47e2
A. Rzhanitsyn <risdeep@gmail.com>
2020-03-16 20:44:59 +0500

    js-asynchronous-programming / lesson №2

---
+++ lessons/js-asynchronous-programming/asynchronous-return.js
@@ -0,0 +1,26 @@
+// sc:
+// https://ru.hexlet.io/courses/js-asynchronous-programming/lessons/asynchronous-return/exercise_unit
+
+// writer.js
+// Реализуйте асинхронную функцию, которая записывает данные по указанному пути и
+// оповещает о завершении работы через переданный колбэк.
+
+// import write from './writer.js';
+
+// write('./myfile', 'data', () => {
+//   console.log('success');
+// });
+
+// Подсказки:
+// для записи в файл используйте функцию fs.writeFile.
+// https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback
+
+// @ts-check
+
+import fs from 'fs';
+
+// BEGIN (write your solution here)
+export default (path, data, callback) => {
+    fs.writeFile(path, data, 'utf8', callback);
+};
+// END
