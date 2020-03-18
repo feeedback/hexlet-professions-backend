87dbaccb8cca0e7ee997d0f5efc54126f57398ff
A. Rzhanitsyn <risdeep@gmail.com>
2020-03-18 19:19:33 +0500

    js-asynchronous-programming / lesson №8

---
+++ lessons/js-asynchronous-programming/promises.js
@@ -0,0 +1,33 @@
+// sc:
+// https://ru.hexlet.io/courses/js-asynchronous-programming/lessons/promises/exercise_unit
+
+// file.js
+// Реализуйте и экспортируйте асинхронную функцию reverse, которая меняет строчки в файле
+// в обратом порядке
+
+// import { reverse } from './file.js';
+
+// // До
+// // one
+// // two
+// reverse(filepath);
+
+// // После
+// // two
+// // one
+
+/* eslint-disable import/prefer-default-export */
+import { promises as fs } from 'fs';
+
+// BEGIN (write your solution here)
+const reverseLines = (data) =>
+    data
+        .split('\n')
+        .reverse()
+        .join('\n');
+
+export const reverse = (filepath) =>
+    fs
+        .readFile(filepath, 'utf8')
+        .then((data) => fs.writeFile(filepath, reverseLines(data), 'utf8'));
+// END
