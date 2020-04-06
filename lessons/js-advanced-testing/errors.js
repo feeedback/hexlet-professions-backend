9527782e8e0956294ece0468b1fa0fc1de9f5949
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-06 11:51:13 +0500

    js-advanced-testing / lesson №1

---
+++ lessons/js-advanced-testing/errors.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+// sc: https://ru.hexlet.io/courses/js-advanced-testing/lessons/errors/exercise_unit
+
+// tests/gt.test.js
+// Напишите негативный тест, проверяющий ошибочные ситуации. Рассмотрите следующие
+// ситуации:
+//    файл не найден
+//    В функцию передан путь до директории на функцию read($filepath).
+
+// read('/undefined'); // boom!
+// read('/etc'); // boom!
+
+// const getFunction = require('../functions');
+
+// const read = getFunction();
+
+// BEGIN
+test('read', () => {
+    expect(() => read('/undefined')).toThrow();
+    expect(() => read('/etc')).toThrow();
+});
+// END
