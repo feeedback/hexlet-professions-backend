42821c1046d1f1c6ab763c51393e440831a101be
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-06 14:29:58 +0500

    js-advanced-testing / lesson №2

--- lessons/js-advanced-testing/fixtures.js
+++ lessons/js-advanced-testing/fixtures.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // sc: https://ru.hexlet.io/courses/js-advanced-testing/lessons/fixtures/exercise_unit
 
 // tests/toHtmlList.test.js
@@ -19,37 +20,25 @@
 // test.each
 // .trim – позволяет удалять концевые пробелы
 
-const { promises: fs } = require('fs');
-const path = require('path');
-const getFunction = require('../functions');
+// const { promises: fs } = require('fs');
+// const path = require('path');
+// const getFunction = require('../functions');
 
-const toHtmlList = getFunction();
+// const toHtmlList = getFunction();
 
 // BEGIN (write your solution here)
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
 
-const data = {
-    inputs: [],
-    result: null,
-};
-
-test('LOG beforeAll', () => {
-    console.log(data);
-});
-
 beforeAll(async () => {
-    const yaml = await readFile('list.yml');
-    const json = await readFile('list.json');
-    const csv = await readFile('list.csv');
-
-    data.inputs.push(yaml, json, csv);
-    data.result = await readFile('result.html');
+    expected = await readFile('result.html');
 });
 
-test.each(data.inputs)('parse files and create ul list of data', (input) => {
-    const list = toHtmlList(input.trim());
-    expect(list).toEqual(data.result.trim());
-});
+const formats = ['csv', 'json', 'yml'];
+test.each(formats)('%s', async (format) => {
+    const filePath = getFixturePath(`list.${format}`);
+    const actual = await toHtmlList(filePath);
 
+    expect(actual.trim()).toEqual(expected.trim());
+});
 // END
