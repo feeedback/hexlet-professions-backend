26b4211f3b8d1293637a923db12436c97e1f111c
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-11 21:57:36 +0500

    regexp / lesson №2

---
+++ lessons/regexp/character_classes.js
@@ -0,0 +1,10 @@
+// sc: https://ru.hexlet.io/courses/regexp/lessons/character_classes/exercise_unit
+
+// solution
+// Напишите регулярное выражение, в котором:
+
+// Первый и второй символ это числа
+// Третий символ это /
+// Четвертый это любой символ, за исключением a-z
+
+/\d\d\/[^a-z]/;
