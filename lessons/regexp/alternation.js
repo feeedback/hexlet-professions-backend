40cf50b7aa6961a51d570b178a5ab2389479a5df
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-11 22:08:17 +0500

    regexp / lesson №4

---
+++ lessons/regexp/alternation.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-expressions */
+// sc: https://ru.hexlet.io/courses/regexp/lessons/alternation/exercise_unit
+
+// Напишите регулярное выражение, которое соответствует подстрокам one или two или
+// three.
+
+/(one|two|three)/;
