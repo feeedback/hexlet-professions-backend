e62c484bab81660d21d93f51e8ecc6bcf89aa066
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-11 21:25:17 +0500

    regexp / lesson №1

---
+++ lessons/regexp/symbols.js
@@ -0,0 +1,7 @@
+// sc: https://ru.hexlet.io/courses/regexp/lessons/symbols/exercise_unit
+
+// solution
+// Напишите регулярное выражение ruby1.*, в котором вместо символа * может
+// находиться любой символ.
+
+/ruby1\../;
