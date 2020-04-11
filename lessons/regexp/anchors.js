c9ac4e8b1593aeca11a0d8e895f9f7d5c30c78b3
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-11 22:05:27 +0500

    regexp / lesson №3

---
+++ lessons/regexp/anchors.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+// sc: https://ru.hexlet.io/courses/regexp/lessons/anchors/exercise_unit
+
+// solution
+// Напишите регулярное выражение, которое соответствует строке support@hexlet.io.
+// Это означает, что такие строки something here support@hexlet.io,
+// support@hexlet.io something here не должны попасть под регулярное выражение.
+
+/^support@hexlet\.io$/;
