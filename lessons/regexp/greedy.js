646c17452f3676312874dd453ce88971d460dabb
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-12 10:17:19 +0500

    regexp / lesson №6

---
+++ lessons/regexp/greedy.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-expressions */
+// sc: https://ru.hexlet.io/courses/regexp/lessons/quantifiers/exercise_unit
+
+// solution
+// Напишите регулярное выражение, которое находит подстроки, состоящие из:
+
+// (
+// Хотя бы один любой символ
+// )
+
+/\(.+?\)/;
