18782e4df6c4f66d8c0fc155d34ac3d9abef4caa
A. Rzhanitsyn <risdeep@gmail.com>
2020-04-26 10:40:04 +0500

    js-arrays / lesson Modification

---
+++ lessons/js-arrays/modification.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+// sc: https://ru.hexlet.io/courses/js-arrays/lessons/modification/exercise_unit
+
+// arrays.js
+// Реализуйте и экспортируйте функцию swap(), которая меняет местами первый и
+// последний элемент массива. Если массив содержит меньше двух элементов, то он
+// возвращается как есть.
+
+// import { swap } from '../arrays';
+
+// swap([]); // []
+// swap([1]); // [1]
+// swap([1, 2]); // [2, 1]
+// swap(['one', 'two', 'three']); // ['three', 'two', 'one']
+// Подсказки
+// Чтобы поменять местами значения, нужно использовать третью переменную
+
+/* eslint-disable no-param-reassign, prefer-destructuring, import/prefer-default-export */
+
+// BEGIN (write your solution here)
+export const swap = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+    const temp = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = temp;
+
+    return arr;
+};
+// END
